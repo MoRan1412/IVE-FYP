52e5a5aed3a08e7d582fa3b9ce3f20e0f78ede7f
MoRan <ooklibaioo@gmail.com>
2024-09-30 14:25:17 +0800

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://moran1412.github.io/ive-fyp/](https://moran1412.github.io/ive-fyp/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/moran1412/ive-fyp** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/moran1412/ive-fyp** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,83 @@
+function avoidObstacle () {
+    if (distance < 500) {
+        avoidDir = 0
+    }
+}
+// Function to receive direction from Raspberry Pi and control motors
+function receiveData () {
+    receivedData = serial.readLine()
+    // Check if data starts with "direction:"
+    if (receivedData.indexOf("direction:") == 0) {
+        // Extract the direction value
+        waterDir = parseFloat(receivedData.substr(10, 5))
+    }
+}
+// Function to control motors based on direction
+function controlMotors (dir: number) {
+    if (dir == 0) {
+        // Move forward
+        basic.showArrow(ArrowNames.North)
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        150,
+        robotbit.Motors.M1B,
+        150
+        )
+    } else if (dir == 1) {
+        // Turn left
+        basic.showArrow(ArrowNames.West)
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        0,
+        robotbit.Motors.M1B,
+        150
+        )
+    } else if (dir == 2) {
+        // Turn right
+        basic.showArrow(ArrowNames.East)
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        150,
+        robotbit.Motors.M1B,
+        0
+        )
+    } else if (dir == 3) {
+        // Move backward
+        basic.showArrow(ArrowNames.South)
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        150,
+        robotbit.Motors.M1B,
+        150
+        )
+    } else {
+        // Stop motors
+        basic.clearScreen()
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        0,
+        robotbit.Motors.M1B,
+        0
+        )
+    }
+}
+let receivedData = ""
+let distance = 0
+let avoidDir = 0
+let waterDir = 0
+// Set up serial communication to USB or pins
+serial.redirectToUSB()
+waterDir = 0
+avoidDir = 0
+basic.showIcon(IconNames.Yes)
+// Main loop
+basic.forever(function () {
+    distance = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P1,
+    PingUnit.MicroSeconds
+    )
+    // Receive movement direction from Raspberry Pi
+    receiveData()
+    basic.pause(200)
+})
